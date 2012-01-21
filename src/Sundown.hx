1f4f404443a11f27337af722e9399e91e18961a4
Skial Bainn <skial.bainn@gmail.com>
2012-01-21 17:50:57 +0000

    cleanup.

--- src/Sundown.hx
+++ src/Sundown.hx
@@ -1,19 +1,19 @@
 package ;
-import cpp.io.File;
 
 /**
  * ...
  * @author Skial Bainn
  */
 
+@:final
+@:require(cpp)
 @:buildXml('<include name="../native.xml"/>')
 @:headerCode('#include "api.h"')
-@:headerClassCode('')
 class Sundown {
 	
 	private static var output:String = '';
 
-	@:functionCode('HxSundown sundown = HxSundown();\n::SunDown_obj::output = sundown.markdown(text);')
+	@:functionCode('HxSundown sundown = HxSundown();\n::Sundown_obj::output = sundown.markdown(text);')
 	public static function markdown(text:String) {
 		return output;
 	}
